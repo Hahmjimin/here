c03c942a8e2d9bc003c28fd0ab6661d74fd073c8
hahmjimin <202110315@bssm.hs.kr>
2021-05-25 22:19:31 +0900

    전공

---
+++ Test.java
@@ -0,0 +1,64 @@
+package 여기다;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int sum=0;
+		sum=Sum(5, 9);
+		System.out.println("sum : "+sum);
+	}
+	public static int Sum(int n1, int n2) {
+		int total = n1+n2;
+		
+		return total;
+	}
+		return 1;
+		int a;
+		double b=1.1
+		String = "abcd";
+		
+		int score = 80;
+		if(score<80) {
+			System.out.println("과목");
+		}
+		else if(score>80) {
+			System.out.println("good");
+		}
+		else {
+			System.out.println("pass");
+		}
+		//////////
+		
+		switch(score) {
+		case 60:{
+			break;
+		}
+		case 70:{
+			break;
+		}
+		default:{
+			System.out.println("예외상황");
+			break;
+		}
+		}
+		///////
+		
+		int i=0;
+		for(;i<10;) {
+			//sysout
+			System.out.println("i : "+i);
+			i++;
+		}
+		int t=0;
+		while(t<10) {
+			System.out.println("t : "+t);
+			t++;
+		}
+		
+		////////
+		Scanner scan = new Scanner(System.in;)
+		System.out.println("Hello World");
+		System.out.println(a);
+	}
+
+}
